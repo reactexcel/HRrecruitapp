94750f69b536e3fb13d6268ef2b30c48c2500288
reactexcel <arun.etech2011@gmail.com>
2018-06-06 15:44:51 +0530

    firebase config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Root } from "native-base";
 import Rootstack from "./config/router";
+import * as firebase from 'firebase';
+import { FIREBASE_CONFIG } from '../src/config/firebase';
+firebase.initializeApp(FIREBASE_CONFIG);
 
 export default class App extends Component {
   render() {
---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase config
+  export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCb_p2o_Y0tTiClPO4FcMHA7_2r0kJppxY",
+    authDomain: "myfirstfirebaseproject-abf19.firebaseapp.com",
+    databaseURL: "https://myfirstfirebaseproject-abf19.firebaseio.com",
+    projectId: "myfirstfirebaseproject-abf19",
+    storageBucket: "myfirstfirebaseproject-abf19.appspot.com",
+    messagingSenderId: "926600885980"
+  }
+
+ 
