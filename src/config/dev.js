15ddd66652c65158cd367bd45026b0e22961120f
reactexcel <arun.etech2011@gmail.com>
2018-06-11 15:10:20 +0530

    Build Error Resloved

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,8 +1,9 @@
 import { GoogleAnalyticsTracker } from "react-native-google-analytics-bridge";
 export const GOOGLE_ANALYTICS_TRACKER = new GoogleAnalyticsTracker("UA-119982957-1");
+let __DEV__ = false;
 let API_URL;
 if (__DEV__) {
-  API_URL = "http://5.9.144.226:3000/exams/";
+  API_URL = "http://api.recruit.excellencetechnologies.in/exams/";
 } else {
   API_URL = "http://5.9.144.226:3000/exams/";
 }
