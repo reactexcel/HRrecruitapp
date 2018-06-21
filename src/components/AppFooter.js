41c6ab646aafbcc4416a18b51b3e2dfc0e1160de
upexcel <rahul.excel2011@gmail.com>
2018-06-21 17:20:30 +0530

    app works

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Footer, Text } from "native-base";
 import VersionNumber from "react-native-version-number";
 import styles from "../styles";
-
+console.log(VersionNumber.bundleIdentifier)
 const AppFooter = () => {
   return (
     <Footer style={styles.footerView}>
       <Text style={styles.footerText}>
-        App Version : {VersionNumber.appVersion}
+        App Version : {VersionNumber.CFBundleVersion}
       </Text>
     </Footer>
   );
