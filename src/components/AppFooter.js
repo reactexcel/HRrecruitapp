ce74c1aac9ef4569ed67feb278b0ea1f8ead8dd3
upexcel <rahul.excel2011@gmail.com>
2018-06-22 12:54:52 +0530

    App Version issue fixed; manually linked react-native-version-number through Xcode

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -2,12 +2,11 @@ import React from "react";
 import { Footer, Text } from "native-base";
 import VersionNumber from "react-native-version-number";
 import styles from "../styles";
-console.log(VersionNumber.bundleIdentifier)
 const AppFooter = () => {
   return (
     <Footer style={styles.footerView}>
       <Text style={styles.footerText}>
-        App Version : {VersionNumber.CFBundleVersion}
+        App Version : {VersionNumber.appVersion}
       </Text>
     </Footer>
   );
