fb58bade9f7c54f5b39e4823e31da67f5f278887
rohit <arun.etech2011@gmail.com>
2018-07-28 11:37:57 +0530

    added new lib for pushnotification

--- android/app/src/main/java/com/hrrecruits/MainApplication.java
+++ android/app/src/main/java/com/hrrecruits/MainApplication.java
@@ -3,6 +3,7 @@ package com.hrrecruits;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -38,6 +39,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new SplashScreenReactPackage(),
             new LinearGradientPackage(),
             new RNFirebasePackage(),
